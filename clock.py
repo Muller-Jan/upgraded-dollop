ad5b822cd87d6a6103444c32f5a3fb56780dbe7c
Jan <81478800+Muller-Jan@users.noreply.github.com>
2021-11-11 21:09:00 +0200

    Update clock.py

--- clock.py
+++ clock.py
@@ -1,6 +1,7 @@
 from telethon import events
 import asyncio
 from collections import deque
+from re import split
 
 
 @borg.on(events.NewMessage(pattern=r"\.clock", outgoing=True))
@@ -32,4 +33,15 @@ async def _(event):
 		await asyncio.sleep(0.1)
 		await event.edit("".join(deq))
 		deq.rotate(1)
-		
+
+@borg.on(events.NewMessage(pattern="\.anim ", outgoing=True))
+async def _(event):
+	if event.fwd_from:
+		return
+	to_anim = split(" ", event.text)[1]
+	deq = deque(list(to_anim))
+	for _ in range(len(to_anim) * 4):
+		await asyncio.sleep(0.1)
+		await event.edit("".join(deq))
+		deq.rotate(1)
+
